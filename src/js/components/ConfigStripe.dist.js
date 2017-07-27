8a27e3f6372380165e5759724d031d5b8ca698c4
korman <korman.yuri@gmail.com>
2017-07-27 23:52:21 +0300

    added stripe page

---
+++ src/js/components/ConfigStripe.dist.js
@@ -0,0 +1,13 @@
+/**
+ * Created by korman on 27.07.17.
+ */
+
+var configStripe = {
+    publicKey: 'pk_test_Rrkex0EsJiXPhtdONdIG9LcX',
+    baseUrl: 'https://coupon.ppcgclub.com',
+    baseFrontUrl: 'https://group-buy.ppcgclub.com'
+};
+
+// var publicKey = 'pk_test_Rrkex0EsJiXPhtdONdIG9LcX';
+// var baseUrl = 'https://coupon.ppcgclub.com';
+// var baseFrontUrl = 'https://group-buy.ppcgclub.com';
